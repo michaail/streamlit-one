47278b5331ca09a492c2778c411ecc759c59c5bf
michalklos <galeo09@gmail.com>
2025-04-26 18:27:34 +0200

    img

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openai import OpenAI
+import base64
 
 key = st.secrets["API_KEY"]
 base_url = st.secrets["BASE_URL"]
@@ -10,26 +11,33 @@ client = OpenAI(
   api_key=key
 )
 
-completion = client.chat.completions.create(
-  extra_body={},
-  model=model,
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "What is in this image?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-          }
-        }
-      ]
-    }
-  ]
-)
+uploaded_file = st.file_uploader("Import image", type=["jpg", "jpeg", "png"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+if uploaded_file is not None:
+  st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+  st.write("Image uploaded successfully!")
+  bytes_data = uploaded_file.getvalue()
+  data = base64.b64encode(bytes_data)
 
-st.write(completion.choices[0].message.content)
+  st.write("## Image Analysis")
+
+  completion = client.chat.completions.create(
+    extra_body={},
+    model=model,
+    messages=[
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "What is in this image?"
+          },
+          {
+            "type": "input_image",
+            "image_url": f"data:image/png;base64,{data}"
+          }
+        ]
+      }
+    ]
+  )
+
+  st.write(completion.choices[0].message.content)

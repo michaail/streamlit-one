a4f87646c5088bf15e2c317e4b44b29e25d5d79f
michalklos <galeo09@gmail.com>
2025-04-26 17:39:27 +0200

    adds pandas thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df

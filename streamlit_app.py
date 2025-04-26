14e693b8bf7577f5e26e2af35116011d726e57e0
michalklos <galeo09@gmail.com>
2025-04-26 17:41:34 +0200

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data

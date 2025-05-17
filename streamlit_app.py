5f191714555364c4dd486e3d68a7728f07284b59
michalklos <galeo09@gmail.com>
2025-05-17 11:23:55 +0200

    adds pdf uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 import base64
+import pdfplumber
 
 key = st.secrets["API_KEY"]
 base_url = st.secrets["BASE_URL"]
@@ -11,37 +12,52 @@ client = OpenAI(
   api_key=key
 )
 
-uploaded_file = st.file_uploader("Import image", type=["jpg", "jpeg", "png"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+def extract_data(feed):
+    data = []
+    with pdfplumber.load(feed) as pdf:
+        pages = pdf.pages
+        for p in pages:
+            data.append(p.extract_tables())
+    return None # build more code to return a dataframe 
+
+with st.sidebar:
+  uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+
+
 if uploaded_file is not None:
-  st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-  st.write("Image uploaded successfully!")
-  # Convert the image to base64
-  bytes_data = uploaded_file.getvalue()
-  base64_image = base64.b64encode(bytes_data).decode('utf-8')
+  df = extract_data(uploaded_file)
+
+# uploaded_file = st.file_uploader("Import image", type=["jpg", "jpeg", "png"], accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+# if uploaded_file is not None:
+#   st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+#   st.write("Image uploaded successfully!")
+#   # Convert the image to base64
+#   bytes_data = uploaded_file.getvalue()
+#   base64_image = base64.b64encode(bytes_data).decode('utf-8')
     
 
-  st.write("## Image Analysis")
+  # st.write("## Image Analysis")
 
-  completion = client.chat.completions.create(
-    extra_body={},
-    model=model,
-    messages=[
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": "What is in this image?"
-          },
-          {
-            "type": "image_url",
-            "image_url": {
-              "url": f"data:image/jpeg;base64,{base64_image}"
-            }
-          }
-        ]
-      }
-    ]
-  )
+  # completion = client.chat.completions.create(
+  #   extra_body={},
+  #   model=model,
+  #   messages=[
+  #     {
+  #       "role": "user",
+  #       "content": [
+  #         {
+  #           "type": "text",
+  #           "text": "What is in this image?"
+  #         },
+  #         {
+  #           "type": "image_url",
+  #           "image_url": {
+  #             "url": f"data:image/jpeg;base64,{base64_image}"
+  #           }
+  #         }
+  #       ]
+  #     }
+  #   ]
+  # )
 
-  st.write(completion.choices[0].message.content)
+  st.write("Application")
